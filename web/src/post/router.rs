dadd140e277f134cbad2af04151750c724b4c1b4
John Davenport <johns10@gmail.com>
2024-09-08 16:33:45 -0400

    Fix edit urls

--- web/src/post/router.rs
+++ web/src/post/router.rs
@@ -9,7 +9,8 @@ use crate::AppState;
 pub fn create_post_router() -> Router<AppState> {
     Router::new()
         .route("/", get(handlers::list_posts).post(handlers::create_post))
-        .route("/:id", get(handlers::edit_post).post(handlers::update_post))
+        .route("/:id", post(handlers::update_post))
+        .route("/:id/edit", get(handlers::edit_post))
         .route("/new", get(handlers::new_post))
         .route("/:id/delete", post(handlers::delete_post))
 }
